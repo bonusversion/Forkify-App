c050a688ec516b7b4854e60bbe5b04dbd0a0adc6
mybonus <506832550@qq.com>
2022-01-08 23:57:52 +0800

    finish

---
+++ src/js/controller.js
@@ -0,0 +1,139 @@
+import * as model from './model.js';
+import { MODAL_CLOSE_SEC } from './config.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import paginationView from './views/paginationView.js';
+import bookmark from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import { mod, recip } from 'prelude-ls';
+import { S } from 'xmlchars/xml/1.0/ed5';
+import resultsView from './views/resultsView.js';
+import bookmarksView from './views/bookmarksView.js';
+
+// https://forkify-api.herokuapp.com/v2
+if (module.hot) {
+  module.hot.accept();
+}
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+    // 0) Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+
+    // 1) Updating bookmarks view
+    bookmarksView.update(model.state.bookmarks);
+
+    // 2) Loading Recipe
+    await model.loadRecipe(id);
+
+    // 3) Rendering Recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    console.log(err);
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    resultsView.renderSpinner();
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) Render results
+    resultsView.render(model.getSearchResultsPage());
+
+    // 4) Render initial pagination buttons
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const conctrolPagination = function (goToPage) {
+  // 1) Render NEW results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 2) Render NEW pagination buttons
+  paginationView.render(model.state.search);
+};
+
+const controlServings = function (newServings) {
+  // Update the recipe servings (in state)
+  model.updateServings(newServings);
+  // Update the recipe view
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
+};
+
+const controlAddBookmark = function () {
+  // 1) Add/Remove bookmark
+  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+  else model.deleteBookmark(model.state.recipe.id);
+
+  // 2) Update recipe view
+  recipeView.update(model.state.recipe);
+
+  // 3) Render bookmarks
+  bookmark.render(model.state.bookmarks);
+};
+
+const controlBookmarks = function () {
+  bookmark.render(model.state.bookmarks);
+};
+
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    // Show loading spinner
+    addRecipeView.renderSpinner();
+
+    // Upload the new recipe data
+    await model.uploadRecipe(newRecipe);
+
+    // Render recipe
+    recipeView.render(model.state.recipe);
+
+    // Success message
+    addRecipeView.renderMessage();
+
+    // Render bookmark View
+    bookmarksView.render(model.state.bookmarks);
+
+    // change ID in URL
+    window.history.pushState(null, '', `#${model.state.recipe.id}`);
+
+    // Close form window
+
+    setTimeout(function () {
+      addRecipeView.toggleWindow();
+      addRecipeView.resetAddRecipeForm();
+    }, MODAL_CLOSE_SEC * 1000);
+
+    // Reset upload Window
+  } catch (err) {
+    addRecipeView.renderError(err.message);
+  }
+};
+
+const init = function () {
+  addRecipeView.addHandlerUpload(controlAddRecipe);
+  bookmarksView.addHandlerRender(controlBookmarks);
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(conctrolPagination);
+  recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
+};
+
+init();
---
+++ src/js/views/bookmarksView.js
@@ -0,0 +1,20 @@
+import View from './View.js';
+import icons from '../../img/icons.svg';
+import previewView from './previewView.js';
+
+class BookmarksView extends View {
+  _parentElement = document.querySelector('.bookmarks');
+  _errorMessage = 'No bookmarks yet. Find a nice recipe and bookmark it :)';
+  _message = '';
+
+  _generateMarkup() {
+    console.log(this._data);
+    return this._data.map(result => previewView.render(result, false)).join('');
+  }
+
+  addHandlerRender(handler) {
+    window.addEventListener('load', handler);
+  }
+}
+
+export default new BookmarksView();
